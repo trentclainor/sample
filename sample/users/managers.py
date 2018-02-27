611ee73356a1ea37441bfd92ec50c293ff271d53
Albert Mikhailov <trent.clainor@gmail.com>
2018-02-27 18:40:36 +0700

    fix

--- sample/users/managers.py
+++ sample/users/managers.py
@@ -4,16 +4,6 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def create_candidate(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_candidate', True)
-        extra_fields.setdefault('is_admin', False)
-        return self.create_user(email, password, **extra_fields)
-
-    def create_recruiter(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_recruiter', True)
-        extra_fields.setdefault('is_admin', False)
-        return self.create_user(email, password, **extra_fields)
-
     def create_user(self, email, password=None, **extra_fields):
         """
         Creates and saves a User with the given name, email and password.
@@ -30,18 +20,26 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    def create_candidate(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_candidate', True)
+        extra_fields.setdefault('is_admin', False)
+        return self.create_user(email, password, **extra_fields)
+
+    def create_recruiter(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_recruiter', True)
+        extra_fields.setdefault('is_admin', False)
+        return self.create_user(email, password, **extra_fields)
+
     def create_superuser(self, email, password, **extra_fields):
         """
         Creates and saves a superuser with the given email and password.
         """
-        extra_fields.setdefault('is_candidate', False)
-        extra_fields.setdefault('is_recruiter', False)
+        extra_fields.setdefault('is_candidate', True)
+        extra_fields.setdefault('is_recruiter', True)
         user = self.create_user(
             email=email,
             password=password,
+            is_admin=True,
+            **extra_fields,
         )
-        user.is_admin = True
-        user.save(using=self._db)
         return user
-
-
